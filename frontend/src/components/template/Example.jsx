fa7ff9f319529782b32c85e03adb454189fd93bf
Taosit <taosit099@gmail.com>
2023-01-26 18:48:48 -0800

    Basic boilerplate for components, layouts and pages

---
+++ frontend/src/components/template/Example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Example.module.css'
+
+const Example = () => {
+  return (
+    <div className={styles.exampleClass}>Template component</div>
+  )
+}
+
+export default Example
