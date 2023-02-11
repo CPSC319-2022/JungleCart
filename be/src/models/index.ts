28d4cfe6a29fc64c413be5ec23fb557cc8f403ea
Jieun Kim <jieun.k@hotmail.com>
2023-02-10 17:35:10 -0800

    api wrapper, structures, export modules

---
+++ be/src/models/index.ts
@@ -0,0 +1,15 @@
+import AdminModel from './admin'
+import CartModel from './cart'
+import LoginModel from './login'
+import OrderModel from './order'
+import ProductModel from './product'
+import UserModel from './user'
+
+export {
+  AdminModel,
+  CartModel,
+  LoginModel,
+  OrderModel,
+  ProductModel,
+  UserModel,
+}
