8469f01d0eed7a56f9bc7023e7919a9fc9d8bfd1
Mark Balantzyan <mgbalant@alumni.ubc.ca>
2023-02-11 16:44:33 -0800

    Changed so browser tab title is 'Jungle Cart', className field safety in Button component added, left/right padding in text input in navbar added

--- frontend/src/components/atoms/button/Button.jsx
+++ frontend/src/components/atoms/button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './Button.module.css'
 
-export const Button = ({children}) => {
+export const Button = ({children, className}) => {
   return (
-    <button className={styles.button}>{children}</button>
+    <button className={`${styles.button} ${className? className : ""}`}>{children}</button>
   )
 }
---
+++ frontend/src/pages/checkout/index.jsx
@@ -0,0 +1,7 @@
+import Navbar from "@/layouts/navbar/Navbar";
+
+const Checkout = () => {
+  return <div><Button>Checkout Page</Button></div>;
+}
+
+export default Checkout;
