6518ed921018a14671247d743dfda7606912e463
Mark Balantzyan <mgbalant@alumni.ubc.ca>
2023-02-12 17:08:31 -0800

    Testing product details code

--- frontend/src/pages/products/index.jsx
+++ frontend/src/pages/products/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import styles from '../../styles/Cart.module.css';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';import { Button } from '@/components/atoms/button/Button';
+import { CardBottom } from '@/components/organisms/cardBottom/CardBottom';
+import { CardTop } from '@/components/organisms/cardTop/CardTop';
+import { ProductCard } from '@/components/organisms/productCard/ProductCard';
+import { ShadedCard } from '@/components/organisms/shadedCard/ShadedCard';
+import crayon from "../../assets/crayon.jpg"
+import styles from "../../styles/ProductDetails.module.css";
+  
 
-const Products = () => {
+const ProductDetails = () => {
   const router = useRouter()
   const { id } = router.query
-
-  return <p>Product: {id}</p>
+  const product = {name: "Crayola 52-0096 96 Crayons, School and Craft Supplies, Gift for Boys and Girls, Kids, Ages 3,4, 5, 6 and Up, Back to school, School supplies, Arts and Crafts, Gifting", 
+  price: 12, img:[crayon], description:"Classic Crayola crayons True hues and intense brightness in a huge variety of colors Kids’ art tools are double wrapped for extra strength includes 96 different crayon colors and built-in sharpener Perfect art tool for arts, crafts and creative fun."}
+  return 
+  <><div className={styles.pagebody}>ShadedCard {...product}
+  <CardTop {...product}></CardTop>
+  <CardBottom className={styles.cardBottom}>
+  </CardBottom>
+  </div>
+  </>
 }
 
-export default Products
+export default ProductDetails
 
