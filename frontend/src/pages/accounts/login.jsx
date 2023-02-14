a4c28b94bf7d74cabfcd39be080cba1c1f9df849
Raphael Deonova <raphaeldeonova@gmail.com>
2023-02-13 18:30:43 -0800

    refactored and added styling

--- frontend/src/pages/accounts/login.jsx
+++ frontend/src/pages/accounts/login.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
-import styles from "./login.module.css"
+import styles from "../../styles/login.module.css"
 import Union from "../../../public/Union.png"
 import Google from "../../../public/Google.png"
 
@@ -17,7 +17,7 @@ const Login = () => {
             <div className={styles.top_container}>
                 <div>
                     <div>
-                        Welcome to Jungle Cart
+                        Welcome to <span className={styles.jungle_cart}>Jungle Cart</span>
                     </div>
                     <div>
                         Amazon buy, sell, for amazon
