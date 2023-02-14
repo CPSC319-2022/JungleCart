db4feb0627ae5ade662527c61e46df555c0010a8
Raphael Deonova <raphaeldeonova@gmail.com>
2023-02-13 20:58:34 -0800

    add layout configs

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,8 +1,20 @@
 import BasicLayout from '@/layouts/basicLayout/BasicLayout';
+import NoNavbarLayout from '@/layouts/noNavbarLayout/noNavbarLayout';
 import Head from 'next/head';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+
+  if(Component.noLayout){
+    return(
+      <>
+      <NoNavbarLayout>
+        <Component {...pageProps} />
+      </NoNavbarLayout>
+    </>
+    )
+  }
+
   return (
     <>
       <BasicLayout>
