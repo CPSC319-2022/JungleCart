f17a8ef7fb7cc7b00086750db9d14d71120f05ed
Jieun Kim <jieun.k@hotmail.com>
2023-02-14 11:15:06 -0800

    deleting dist on remote

--- be/dist/utils/errorGenerator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var errorGenerator = function (obj) {
-    var error = new Error(obj.message);
-    error.statusCode = obj.statusCode;
-    throw error;
-};
-exports.default = errorGenerator;
--- be/dist/utils/types.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-function User(user, User) {
-    throw new Error('Function not implemented.');
-}
-exports.User = User;
