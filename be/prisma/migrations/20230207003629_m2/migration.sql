82e4c86dacd70c4b7ca3fb3edb89d4505fb396d2
Jieun Kim <jieun.k@hotmail.com>
2023-02-14 22:59:53 -0800

    remove prisma dir

--- be/prisma/migrations/20230207003629_m2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `shipping` on the `Cart_item` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Cart_item` DROP COLUMN `shipping`;
