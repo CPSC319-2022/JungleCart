def7b296bbd51c41b3f1ca54e3cb03572ccb43d6
yunmiju <yunmiju@students.cs.ubc.ca>
2023-02-15 16:45:58 -0800

    fixed @prisma tags

--- be/src/wrapper/order.wrapper.ts
+++ be/src/wrapper/order.wrapper.ts
@@ -1,5 +1,5 @@
 import e, { Request, Response } from 'express'
-import { OrderModel } from '../models'
+// import { OrderModel } from '../models'
 import { Order_combo } from '../utils/returnMapper'
 import { Order_item } from '../utils/types'
 
