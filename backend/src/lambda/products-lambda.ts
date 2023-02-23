86bea89fc455ba4fee937e762b9b7b6c86b01e01
Ethan Thoma <ethanthoma@gmail.com>
2023-02-22 19:36:58 -0800

    Cdk backend (#51)
    * renamed be, added cdk, set-up lambda with get-products
    
    * added db stack
    
    * workong on lambda calling rds
    
    * restructured with best practices, has BUILD issues
    
    * fixed subnet issue
    
    * 'refactoring'
    
    * auth stack setup
    
    * fixed auth dependency issues
    
    * added routing to lambdas, resources for api, and better building; fixed auth bug; changed lambda to be typescript
    
    * updated status codes, refactored some error checks
    
    * fix dir names and improved building
    
    ---------
    
    Co-authored-by: Armin Talaie <talaiearmin78@gmail.com>

---
+++ backend/src/lambda/products-lambda.ts
@@ -0,0 +1,109 @@
+// noinspection SqlNoDataSourceInspection
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {query, Router} = require("/opt/nodejs/node_modules/sql-layer");
+
+const router = new Router(process.env as { [key: string]: string });
+
+router.post('/', addProduct);
+router.delete('/:productId', deleteProductById);
+router.get('/:productId', getProductInfoById);
+router.put('/:productId', updateProductInfoById);
+router.get('/', getProductsInfo);
+
+exports.handler = async function (event) {
+    return await router.route(event);
+};
+
+async function addProduct(event) {
+    if (!validateProductInformation(event.body)) {
+        return {
+            statusCode: 422,
+            body: "addProduct - Invalid product information"
+        };
+    }
+
+    const prod = event.body;
+    const sql = 'INSERT INTO prod.product SET ?';
+
+    return query(sql, [prod])
+        .then((results) => ({
+            statusCode: 201,
+            body: results
+        }))
+        .catch((error) => ({
+            statusCode: error.statusCode,
+            body: error.message,
+        }));
+}
+
+async function deleteProductById(event) {
+    if (!event.body.id)
+        return {statusCode: 400, body: 'deleteProductById - No id'};
+
+    const id = event.body.id;
+    const sql = 'DELETE FROM prod.product WHERE id = ?';
+
+    return query(sql, [id])
+        .then((results) => ({
+            statusCode: 204,
+            body: results
+        }))
+        // todo check type of error if it is from invalid id (404) or connection error (599)
+        .catch((error) => ({
+            statusCode: error.statusCode,
+            body: error.message,
+        }));
+}
+
+async function getProductInfoById(event) {
+    if (!event.body.id)
+        return {statusCode: 400, body: 'getProductInfoById - No id'};
+
+    const id = event.body.id;
+    const sql = 'SELECT * FROM prod.product WHERE id = ?';
+
+    return query(sql, [id])
+        .then((results) => ({
+            statusCode: 200,
+            body: results
+        }))
+        // todo check type of error if it is from invalid id (404) or connection error (599)
+        .catch((error) => ({
+            statusCode: error.statusCode,
+            body: error.message,
+        }));
+}
+
+async function updateProductInfoById(event) {
+    if (!event.body.id || !event.body.info)
+        return {statusCode: 400, body: 'updateProductInfoById - No id or info'};
+    if (!validateProductInformation(event.body.info))
+        return {statusCode: 422, body: "updateProductInfoById - Invalid product information"};
+
+    const info = event.body.info;
+    const id = event.body.id;
+    const sql = 'UPDATE prod.product SET ? WHERE id = ?';
+
+    return query(sql, [info, id])
+        .then((results) => ({
+            statusCode: 200,
+            body: results
+        }))
+        // todo check type of error if it is from invalid id (404) or connection error (599)
+        .catch((error) => ({
+            statusCode: error.statusCode,
+            body: error.message,
+        }));
+}
+
+async function getProductsInfo(event) {
+    const searchOpt = event.body.searchOpt;
+    const order = event.body.order;
+    const pageOpt = event.body.pageOpt;
+    return {statusCode: 300, body: ''};
+}
+
+// todo implement
+function validateProductInformation(prod) {
+    return prod === prod;
+}
