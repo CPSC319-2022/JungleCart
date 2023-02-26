650d53ab266da448d4b5eeac68eda8162f02f3a1
yunmiju <yunmiju@students.cs.ubc.ca>
2023-02-25 18:59:52 -0800

    fixed order class in be

--- be/src/models/cart.ts
+++ be/src/models/cart.ts
@@ -1,25 +1,31 @@
-import {connection, query} from '../utils/db';
+import { connection, query } from '../utils/db';
 
 class CartModel {
   public async addCartItem(info) {
-    const conn = await connection()
-    const sql = 'INSERT INTO cart_item SET ?'
-    const newUser = await query(conn, sql, [info])
-    return newUser
+    const conn = await connection();
+    const sql = 'INSERT INTO cart_item SET ?';
+    const newUser = await query(conn, sql, [info]);
+    return newUser;
   }
 
   public async findAllCartItems() {
-    const conn = await connection()
-    const sql = 'SELECT * FROM cart_item'
-    const users = await query(conn, sql)
-    return users
+    const conn = await connection();
+    const sql = 'SELECT * FROM cart_item';
+    const users = await query(conn, sql);
+    return users;
   }
 
-  public async deleteCartItemById(id) {}
+  public async deleteCartItemById(id) {
+    //
+  }
 
-  public async getCartItems(id) {}
+  public async getCartItems(id) {
+    //
+  }
 
-  public async updateCartItems(id, info) {}
+  public async updateCartItems(id, info) {
+    //
+  }
 }
 
-export default new CartModel()
+export default new CartModel();
