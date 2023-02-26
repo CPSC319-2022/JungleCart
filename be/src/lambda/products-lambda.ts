0b669d2083835d0b39f8c74efb1f78eb81a00ef5
yunmiju <yunmiju@students.cs.ubc.ca>
2023-02-26 15:03:28 -0800

    before merge to jc-66

--- be/src/lambda/products-lambda.ts
+++ be/src/lambda/products-lambda.ts
@@ -1,22 +1,26 @@
 // noinspection SqlNoDataSourceInspection
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { query, Router } = require('/opt/nodejs/node_modules/sql-layer')
+// const { query, Router } = require('/opt/nodejs/node_modules/sql-layer')
+import {
+  query,
+  Router,
+  createConnection,
+} from '/opt/nodejs/node_modules/sql-layer';
+const router = new Router(process.env as { [key: string]: string });
 
-const router = new Router(process.env as { [key: string]: string })
-
-router.post('/', addProduct)
-router.delete('/:productId', deleteProductById)
-router.get('/:productId', getProductInfoById)
-router.put('/:productId', updateProductInfoById)
-router.get('/', getProductsInfo)
+router.post('/', addProduct);
+router.delete('/:productId', deleteProductById);
+router.get('/:productId', getProductInfoById);
+router.put('/:productId', updateProductInfoById);
+router.get('/', getProductsInfo);
 
 exports.handler = async function (event) {
-  return await router.route(event)
-}
+  return await router.route(event);
+};
 
 async function addProduct(event) {
-  const prod = event.body
-  const sql = 'INSERT INTO backend.product SET ?'
+  const prod = event.body;
+  const sql = 'INSERT INTO backend.product SET ?';
 
   return query(sql, [prod])
     .then((results) => ({
@@ -26,14 +30,14 @@ async function addProduct(event) {
     .catch((error) => ({
       statusCode: 400,
       body: error.message,
-    }))
+    }));
 }
 
 async function deleteProductById(event) {
-  if (!event.body.id) return { statusCode: 400, body: 'no id' }
+  if (!event.body.id) return { statusCode: 400, body: 'no id' };
 
-  const id = event.body.id
-  const sql = 'DELETE FROM product WHERE id = ?'
+  const id = event.body.id;
+  const sql = 'DELETE FROM product WHERE id = ?';
 
   return query(sql, [id])
     .then((results) => ({
@@ -43,14 +47,14 @@ async function deleteProductById(event) {
     .catch((error) => ({
       statusCode: 400,
       body: error.message,
-    }))
+    }));
 }
 
 async function getProductInfoById(event) {
-  if (!event.body.id) return { statusCode: 400, body: 'no id' }
+  if (!event.body.id) return { statusCode: 400, body: 'no id' };
 
-  const id = event.body.id
-  const sql = 'SELECT * FROM backend.product WHERE id = ?'
+  const id = event.body.id;
+  const sql = 'SELECT * FROM backend.product WHERE id = ?';
 
   return query(sql, [id])
     .then((results) => ({
@@ -60,16 +64,16 @@ async function getProductInfoById(event) {
     .catch((error) => ({
       statusCode: 400,
       body: error.message,
-    }))
+    }));
 }
 
 async function updateProductInfoById(event) {
   if (!event.body.id || !event.body.info)
-    return { statusCode: 400, body: 'no id or info' }
+    return { statusCode: 400, body: 'no id or info' };
 
-  const info = event.body.info
-  const id = event.body.id
-  const sql = 'UPDATE product SET ? WHERE id = ?'
+  const info = event.body.info;
+  const id = event.body.id;
+  const sql = 'UPDATE product SET ? WHERE id = ?';
 
   return query(sql, [info, id])
     .then((results) => ({
@@ -79,12 +83,12 @@ async function updateProductInfoById(event) {
     .catch((error) => ({
       statusCode: 400,
       body: error.message,
-    }))
+    }));
 }
 
 async function getProductsInfo(event) {
-  const searchOpt = event.body.searchOpt
-  const order = event.body.order
-  const pageOpt = event.body.pageOpt
-  return { statusCode: 300, body: '' }
+  const searchOpt = event.body.searchOpt;
+  const order = event.body.order;
+  const pageOpt = event.body.pageOpt;
+  return { statusCode: 300, body: '' };
 }
--- be/src/models/admin.ts
+++ be/src/models/admin.ts
@@ -1,18 +1,18 @@
-import {connection, query} from '../utils/db';
-import * as dto from '../utils/types.dto';
+import { connection, query } from '../utils/db';
+import { User } from '../utils/types';
 
 export async function addAdmin(info) {
-  const conn = await connection()
-  const sql = 'INSERT INTO admin SET ?'
-  const newUser = await query(conn, sql)
-  return newUser
+  const conn = await connection();
+  const sql = 'INSERT INTO admin SET ?';
+  const newUser = await query(conn, sql);
+  return newUser;
 }
 
 export async function findAllAdmins() {
-  const conn = await connection()
-  const sql = 'SELECT * FROM admin'
-  const users = await query(conn, sql)
-  return users
+  const conn = await connection();
+  const sql = 'SELECT * FROM admin';
+  const users = await query(conn, sql);
+  return users;
 }
 
 class AdminModel {
@@ -21,25 +21,71 @@ class AdminModel {
   }
 
   public async getAdminInfoById(adminId) {
-    return {}
+    const conn = await connection();
+    const sql = 'SELECT * FROM admin WHERE id = ?';
+    const ci = await query(conn, sql, [adminId]);
+    return ci;
   }
 
   public async getUsers() {
-    return {}
+    const conn = await connection();
+    const sql = `SELECT JSON_OBJECT(
+    'admin', (SELECT JSON_ARRAYAGG(JSON_OBJECT(
+                'id', a.id,
+                'first_name', a.first_name,
+                'last_name', a.last_name,
+                'department', a.department,
+                'email', a.email
+            )) FROM admin a),
+    'user', (SELECT JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id', u.id,
+                'first_name', u.first_name,
+                'last_name', u.last_name,
+                'department', u.department,
+                'email', u.email
+                )) FROM user u)) as combined`;
+    const users = await query(conn, sql);
+    return JSON.parse((users as object)[0].combined);
   }
 
-  public async addUser(user: dto.User) {
-    console.log(user)
-    return {}
+  public async addUser(user: User) {
+    const conn = await connection();
+    const sql = 'INSERT INTO user SET ?';
+    const nu = await query(conn, sql, [user]);
+    return nu;
   }
 
-  public async isEmailExist(email: string) {
-    return true
+  public async getAdminDashboard(id) {
+    const conn = await connection();
+    //
   }
 
   public async addAdmins(info) {
-    return true
+    return true;
+  }
+
+  public async isEmailExist(email) {
+    const conn = await connection();
+    const sql = 'SELECT COUNT(*) FROM user WHERE email = ?';
+    const rst = await query(conn, sql, [email]);
+    return rst;
+  }
+
+  public async isIdExist(uid) {
+    const conn = await connection();
+    const sql = 'SELECT COUNT(*) FROM user WHERE id = ?';
+    const rst = await query(conn, sql, [uid]);
+    return rst;
+  }
+
+  public async deleteUserById(uid) {
+    const conn = await connection();
+    const sql = 'DELETE FROM user WHERE id = ?';
+    const nu = await query(conn, sql, [uid, uid]);
+    console.log(nu);
+    return nu;
   }
 }
 
-export default new AdminModel()
+export default new AdminModel();
