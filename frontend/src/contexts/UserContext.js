cb1f347a696699d1db8391ed52980c252b628b19
Armin Talaie <talaiearmin78@gmail.com>
2023-02-26 16:04:45 -0800

    pretiter

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -6,28 +6,28 @@ const useUserContext = () => {
   return useContext(UserContext);
 };
 
-
 const UserContextProvider = ({ children }) => {
   const [user, setCurrUser] = useState({
-    id : 0,
-    first_name: "",
-    last_name: "",
-    email: ""
+    id: 0,
+    first_name: '',
+    last_name: '',
+    email: '',
   });
 
   const setUser = (id, first_name, last_name, email) => {
     setCurrUser({
-        id : id,
-        first_name: first_name,
-        last_name: last_name,
-        email: email
-    })
-  }
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+    });
+  };
 
   return (
     <UserContext.Provider
       value={{
-        user, setUser
+        user,
+        setUser,
       }}
     >
       {children}
