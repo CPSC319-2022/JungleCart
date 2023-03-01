16b5671892b10c1b75d1e7f124cd9dc647f07eaf
Raphael Deonova <raphaeldeonova@gmail.com>
2023-02-28 19:48:17 -0800

    added mock data

---
+++ frontend/src/seeds/addresses.js
@@ -0,0 +1,36 @@
+export const addresses = {
+	addresses: {
+		preferred: {
+			id: 101,
+			address_line1: "510 W Georgia St",
+			address_line2: "#14",
+			city: "Vancouver",
+			province: "BC",
+			postal_code: "V6B 0M3",
+			recipient: "Recipient 1",
+			telephone: "(604) 643-5500",
+			},
+		others: [
+            {
+                id: 101,
+                address_line1: "510 W Georgia St",
+                address_line2: "#14",
+                city: "Vancouver",
+                province: "BC",
+                postal_code: "V6B 0M3",
+                recipient: "Recipient 2",
+                telephone: "(604) 643-5500",
+            },
+            {
+                id: 101,
+                address_line1: "510 W Georgia St",
+                address_line2: "#14",
+                city: "Vancouver",
+                province: "BC",
+                postal_code: "V6B 0M3",
+                recipient: "Recipient 3",
+                telephone: "(604) 643-5500",
+            }
+	    ]    
+    }
+}
