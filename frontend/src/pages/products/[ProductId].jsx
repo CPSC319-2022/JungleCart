89fd214b6a57036c4b76cae167c55163a858bc47
Mark Balantzyan <mgbalant@alumni.ubc.ca>
2023-03-09 23:31:21 -0800

    Revised check user/seller author syntax, adjusted CSS styling

--- frontend/src/pages/products/[ProductId].jsx
+++ frontend/src/pages/products/[ProductId].jsx
@@ -14,7 +14,7 @@ const ProductDetails = () => {
   const router = useRouter();
   const ProductId = router.query.ProductId;
   
-  const isAuth = false;
+  const isAuthor = false;
 
   useEffect(() => {
     if (!router.query?.productId) return;
@@ -50,57 +50,48 @@ const ProductDetails = () => {
         <div className={styles.belowbar}>
 
           <h1>{products.products[ProductId].name}</h1>
-          <button onClick={() => onSubmit()}>Add to Cart</button>
-
-        </div>
-
-        <div className={styles.pricebox}>
-          <h2>$12</h2>
-        </div>
-        <div className={styles.pagebody}>
-          <Image src={products.products[ProductId].img[0]} alt='' width={400} height={300} style={{ objectFit: "contain" }} unoptimized={true} />
-          <div className={styles.info}>
-            <ul>
-              {products.products[ProductId].description.split('. ').map(sentence => <li>{sentence}</li>)}
-            </ul>
-            <table className={styles.table}>
-              <thead>
-
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Shipping</td>
-                  <td>Shipped only in BC</td>
-                </tr>
-                <tr>
-                  <td>Seller</td>
-                  <td>Product seller id/name</td>
-                </tr>
-                <tr>
-                  <td>Status</td>
-                  <td> {products.products[ProductId].status}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      
-          <div className={styles.belowbar}>
-
-
-            &nbsp;&nbsp;&nbsp;
-            {isAuth && <>
+          {isAuthor? <>
             <button className={Button} onClick={() => onSubmit()}>Edit</button>
             &nbsp;&nbsp;&nbsp;
             <button className={Button} onClick={() => onSubmit()}>Delete</button>
             </>
-            }
-          </div>
+            :
+            <><button onClick={() => onSubmit()}>Add to Cart</button></>
+          }
+        </div>
+
+          <div className={styles.pricebox}>
+              <h2>$12</h2>
+            </div><div className={styles.pagebody}>
+              <Image src={products.products[ProductId].img[0]} alt='' width={400} height={300} style={{ objectFit: "contain" }} unoptimized={true} />
+              <div className={styles.info}>
+                <ul>
+                  {products.products[ProductId].description.split('. ').map(sentence => <li>{sentence}</li>)}
+                </ul>
+                <table className={styles.table}>
+                  <thead>
+
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Shipping</td>
+                      <td>Shipped only in BC</td>
+                    </tr>
+                    <tr>
+                      <td>Seller</td>
+                      <td>Product seller id/name</td>
+                    </tr>
+                    <tr>
+                      <td>Status</td>
+                      <td> {products.products[ProductId].status}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
       </section>
     </article></>
   )
-
-
 
 }
 
