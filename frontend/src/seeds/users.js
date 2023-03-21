076a145bf344ec75a29f0fd369b0c87c514aa8c3
Raphael Deonova <56807018+raphaeldeonova@users.noreply.github.com>
2023-03-20 19:32:26 -0700

    JC: 36 user profile and settings (#92)
    * modified gitignore for only backend files
    
    * added icons for profile page
    
    * added mock data
    
    * changed warning and error colors to match figma
    
    * added profile page
    
    * added modal for editing the
    
    * resolved build error
    
    * changed profile info styling
    
    * add handler stubs
    
    * added add address functionality
    
    * added edit address modals
    
    * add remove address functionality
    
    * add Payment edit and add functionality
    
    * changed button color, fix event propagation bug
    
    * ran prettier

---
+++ frontend/src/seeds/users.js
@@ -0,0 +1,74 @@
+export const users = [
+    {
+        "id": 1,
+        "is_admin": 0,
+        "first_name": "Billy",
+        "last_name": "Bob",
+        "email": "billybob@gmail.com",
+        "department_id": 1,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 2,
+        "is_admin": 0,
+        "first_name": "James",
+        "last_name": "Johnson",
+        "email": "jamesjohnson@gmail.com",
+        "department_id": 2,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 3,
+        "is_admin": 0,
+        "first_name": "David",
+        "last_name": "Dirk",
+        "email": "daviddirk@gmail.com",
+        "department_id": 3,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 4,
+        "is_admin": 0,
+        "first_name": "Sarah",
+        "last_name": "Smith",
+        "email": "sarahsmith@gmail.com",
+        "department_id": 4,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 5,
+        "is_admin": 0,
+        "first_name": "Michael",
+        "last_name": "Miller",
+        "email": "michaelmiller@gmail.com",
+        "department_id": 5,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 6,
+        "is_admin": 0,
+        "first_name": "Emily",
+        "last_name": "Evans",
+        "email": "emilyevans@gmail.com",
+        "department_id": 6,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 7,
+        "is_admin": 0,
+        "first_name": "Andrew",
+        "last_name": "Adams",
+        "email": "andrewadams@gmail.com",
+        "department_id": 7,
+        "created_at": "2023-02-26T00:00:00.000Z"
+    },
+    {
+        "id": 8,
+        "is_admin": 1,
+        "first_name": "admin",
+        "last_name": "admin",
+        "email": "admin@gmail.com",
+        "department_id": 1,
+        "created_at": "2023-03-01T00:00:00.000Z"
+    }
+]
