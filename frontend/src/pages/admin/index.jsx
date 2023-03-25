548a27b6499c756a30b258f6d3af61093f9c0459
Jingxuan <taosit099@gmail.com>
2023-03-25 14:15:08 -0700

    Tweaked admin page
    Implemented search.
    Sorted users firstly by is_admin, then by first name.
    Fixed scrolling issue and adjusted responsiveness

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -1,78 +1,88 @@
-import Separator from "@/components/atoms/separator/Separator";
-import React, {useEffect, useState} from "react";
-import styles from "./Admin.module.css"
-import { users as seedusers }  from "@/seeds/users" ;
-import { Button } from "@/components/atoms/button/Button";
-import { useRouter } from "next/router";
+import Separator from '@/components/atoms/separator/Separator';
+import React, { useEffect, useState } from 'react';
+import styles from './Admin.module.css';
+import { users as seedusers } from '@/seeds/users';
+import { useRouter } from 'next/router';
 
 const Admin = () => {
-    const router = useRouter();
-    const [users, setUsers] = useState(seedusers)
-    const [searchText, setSearchText] = useState("")
+  const router = useRouter();
+  const [users, setUsers] = useState(seedusers);
+  const [searchText, setSearchText] = useState('');
 
-    useEffect(() => {
-        //fetch users
-    }, [])
+  useEffect(() => {
+    //fetch users
+  }, []);
 
-    const handleRemove = (e, user_id) => {
-        e.stopPropagation();
-        console.log(user_id)
-    }
+  const searchedUsers = users
+    .filter((user) => {
+      return (
+        user.first_name.toLowerCase().includes(searchText.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(searchText.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchText.toLowerCase())
+      );
+    })
+    .sort((a, b) => {
+      if (a.is_admin) return -1;
+      if (b.is_admin) return 1;
+      if (a.first_name < b.first_name) return -1;
+      return 1;
+    });
 
-    const handleUserClick = (user_id) => {
-        router.push("admin/viewuser/" + user_id)
-    }
+  const handleRemove = (e, user_id) => {
+    e.stopPropagation();
+    console.log(user_id);
+  };
 
-    const handleSearch = () => {
-        console.log(searchText)
-    }
+  const handleUserClick = (user_id) => {
+    router.push('admin/viewuser/' + user_id);
+  };
 
-    return(
-        <main>
-            <section>
-                <div className="section-header">
-                    Users
-                </div>
-                <Separator />
-                <div className="flex gap-3 w-full">
-                    <input type="text" placeholder="Search for users..." className="input input-bordered grow" onChange={(e) => setSearchText(e.target.value)}/>
-                    <div className="flex">
-                        <Button onClick={handleSearch}>
-                            Search
-                        </Button>
+  return (
+    <main className={styles.container}>
+      <section>
+        <div className="section-header pl-2">Users</div>
+        <Separator />
+        <div className="flex gap-3 w-full pb-4">
+          <input
+            type="text"
+            placeholder="Search for users..."
+            className="input input-bordered grow"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <div className={styles.scrollable}>
+          <ul className="list-none h-auto flex flex-col divide-y divide-gray-medium">
+            {searchedUsers.map((user) => {
+              return (
+                <li
+                  key={user.id}
+                  className={` flex w-full justify-between p-2 md:p-4 ${styles.userlist_item}`}
+                  onClick={() => handleUserClick(user.id)}
+                >
+                  <div className="flex flex-col">
+                    <div className="font-bold text-xl flex">
+                      {user.first_name} {user.last_name}
+                      {user.is_admin == 1 && (
+                        <div className="ml-2 pt-1 font-bold text-sm text-primary-dark">
+                          Admin
+                        </div>
+                      )}
                     </div>
-                </div>
-                <div className="overflow-y-auto w-full mt-8">
-                    <ul className="list-none flex flex-col divide-y divide-gray-medium max-h-144 overflow-y-auto">
-                        {users.map((user) => {
-                            console.log(user.is_admin == 1)
-                            return (
-                              <li
-                                key={user.id}
-                                className={` flex w-full justify-between p-5 ${styles.userlist_item}`}
-                                onClick={() => handleUserClick(user.id)}
-                              >
-                                <div className="flex flex-col">
-                                  <div className="font-bold text-xl flex">
-                                    {user.first_name} {user.last_name}
-                                    {user.is_admin == 1 && 
-                                    <div className="ml-2 font-bold text-sm text-primary-dark">
-                                      Admin
-                                    </div>}
-                                  </div>
-                                  <div>{user.email}</div>
-                                </div>
-                                <div className="font-bold text-error cursor-pointer flex items-center" onClick={(e) => handleRemove(e, user.id)}>
-                                  Remove
-                                </div>
-                              </li>
-                            );
-                        })}
-
-                    </ul>
-                </div>
-            </section>
-        </main>
-    )
-}
-export default Admin
+                    <div>{user.email}</div>
+                  </div>
+                  <div
+                    className="font-bold text-error cursor-pointer flex items-center"
+                    onClick={(e) => handleRemove(e, user.id)}
+                  >
+                    Remove
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Admin;
