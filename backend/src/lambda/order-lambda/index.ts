78f995237034a39c293c9b3dc368e2480facf929
Yunmi Ju <yunmiju0218@gmail.com>
2023-03-25 15:47:55 -0700

    Backend/cart+admin (#105)
    * fixed @prisma tags
    
    * query get/update/add
    
    * implementing cart-stack.ts and carts-lambda.ts
    
    * before merge
    
    * fixed order class in be
    
    * before merge to jc-66
    
    * before merge to jc-66
    
    * before merge main - changed cart/admin lambda
    
    * before merge
    
    * fixed merge conflict
    
    * fixed importing style in app.ts
    
    * Revert "fixed importing style in app.ts"
    
    This reverts commit 628eff938285c09b48b8154ed195ca6fedf262aa.
    
    * Revert "Revert "fixed importing style in app.ts""
    
    This reverts commit 196758460bfc857a3e07f7a3b1ffdb2299bb5680.
    
    * working on test/before merge
    
    * refactoring
    
    * finished merging JC-96-2
    
    * add feature(cart - duplicated post) / changed to show error msg
    
    * Update backend/package.json
    
    backend/package.json script; deploy: macos_build_node_layer -> build_node_layer
    
    Co-authored-by: Armin Talaie <talaiearmin78@gmail.com>
    
    * Update backend/package.json
    
    move chai-http, fs-extra to dev-dep
    
    Co-authored-by: Armin Talaie <talaiearmin78@gmail.com>
    
    ---------
    
    Co-authored-by: yunmiju <yunmiju@students.cs.ubc.ca>
    Co-authored-by: Armin Talaie <talaiearmin78@gmail.com>

--- backend/src/lambda/order-lambda/index.ts
+++ backend/src/lambda/order-lambda/index.ts
@@ -1,34 +1,31 @@
-import { SQLManager } from '/opt/common/SQLManager';
-import { IOrder } from "@/lambdas/order-lambda/orderRequest";
-SQLConnectionManager.connect(true);
+// import { SQLManager } from '/opt/common/SQLManager';
+// import { IOrder } from "@/lambdas/order-lambda/orderRequest";
+// SQLConnectionManager.connect(true);
 
-// handles routing and sends request
-exports.handler = async function (event) {
-  return handleOrder(event);
-};
+// // handles routing and sends request
+// exports.handler = async function (event) {
+//   return handleOrder(event);
+// };
 
-// handlers
-export async function handleOrder(event): Promise<any> {
-  const request = event as IOrder;
-  console.log(request);
+// // handlers
+// export async function handleOrder(event): Promise<any> {
+//   const request = event as IOrder;
+//   console.log(request);
 
-
-
-  // let userEmail;
-  // try {
-  //   userEmail = event.request.userAttributes.email;
-  // } catch (e) {
-  //   throw new Error('Missing Email');
-  // }
-  // const sql = 'SELECT email FROM dev.user WHERE email = ?';
-  //
-  // const response = await SQLConnectionManager.query(sql, [userEmail]);
-  // const rows = response as any[];
-  // if (rows.length > 0) {
-  //   return event;
-  // } else {
-  //   console.log(`${userEmail} not in db`);
-  //   throw new Error('NOT USER');
-  // }
-}
-
+// let userEmail;
+// try {
+//   userEmail = event.request.userAttributes.email;
+// } catch (e) {
+//   throw new Error('Missing Email');
+// }
+// const sql = 'SELECT email FROM dev.user WHERE email = ?';
+//
+// const response = await SQLConnectionManager.query(sql, [userEmail]);
+// const rows = response as any[];
+// if (rows.length > 0) {
+//   return event;
+// } else {
+//   console.log(`${userEmail} not in db`);
+//   throw new Error('NOT USER');
+// }
+// }
--- backend/tests/unit/lambda/order-lambda/index.test.ts
+++ backend/tests/unit/lambda/order-lambda/index.test.ts
@@ -1,24 +1,24 @@
-import * as data from 'tests/events/event-order-1.json';
-import * as chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import { expect } from 'chai';
-chai.use(chaiAsPromised);
-import * as sinon from 'sinon';
-import { SQLManager } from '/opt/common/SQLManager';
-import { handleOrder } from "@/lambdas/order-lambda";
+// import * as data from 'tests/events/event-order-1.json';
+// import * as chai from 'chai';
+// import chaiAsPromised from 'chai-as-promised';
+// import { expect } from 'chai';
+// chai.use(chaiAsPromised);
+// import * as sinon from 'sinon';
+// import { SQLManager } from '/opt/common/SQLManager';
+// import { handleOrder } from "@/lambdas/order-lambda";
 
-describe('Unit tests for Authentication', function () {
-  let stub;
+// describe('Unit tests for Authentication', function () {
+//   let stub;
 
-  before(() => {
-    stub = sinon.stub(SQLConnectionManager, 'createConnection');
-  });
-  it('should throw an error if user email is missing from event', async () => {
-    const event = data;
-    expect(handleOrder(event)).to.eventually.be.rejected;
-  });
+//   before(() => {
+//     stub = sinon.stub(SQLConnectionManager, 'createConnection');
+//   });
+//   it('should throw an error if user email is missing from event', async () => {
+//     const event = data;
+//     expect(handleOrder(event)).to.eventually.be.rejected;
+//   });
 
-  after(() => {
-    stub.restore();
-  });
-});
+//   after(() => {
+//     stub.restore();
+//   });
+// });
