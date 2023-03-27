5df74dbaa9a2ad257ad5ee1b1c8bb998056fad8f
Jingxuan <taosit099@gmail.com>
2023-03-27 14:26:10 -0700

    Tested products endpoint

--- frontend/src/pages/products/index.jsx
+++ frontend/src/pages/products/index.jsx
@@ -4,33 +4,30 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { SortAndFilter } from '@/components/organisms/sortAndFilter/SortAndFilter';
 import { useUserContext } from '@/contexts/UserContext';
-// import {products} from '@/seeds/products';
+
 const Products = () => {
   const { user } = useUserContext();
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
-  //console.log(products);
   const { push, query } = useRouter();
 
   useEffect(() => {
     if (!query.page) {
       push({ query: { ...query, page: 1 } }, undefined, { shallow: true });
     }
-    fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/products?` +
-        new URLSearchParams({
-          search: query.search || '',
-          sort: query.sort || '',
-          category: query.category || '',
-          page,
-        }),
-      {
-        headers: { Authentication: `Bearer ${user.accessToken}` },
-      }
-    )
+    // console.log(user.accessToken);
+    const url =
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}products?` +
+      new URLSearchParams({
+        search: query.search || '',
+        sort: query.sort || '',
+        // category: query.category || '',
+        page,
+      });
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setProducts(data.products);
+        setProducts(data);
       });
   }, [query, page, push]);
 
