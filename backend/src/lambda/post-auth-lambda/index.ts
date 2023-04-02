5d8bf411a603384c217646482640bdb95b79f79d
Jieun Kim <jieun.k@hotmail.com>
2023-04-01 22:55:22 -0700

    exclude files not used currently

--- backend/src/lambda/post-auth-lambda/index.ts
+++
@@ -1,8 +0,0 @@
-// import Router, { ResponseContent } from '/opt/core/Router';
-// import UserController from './UserController';
-// import QueryBuilder from 'opt/core/query-builder';
-// import SQLManager from 'opt/core/SQLManager';
-
-// exports.handler = (event) => {
-//   const email = event.request.userAttributes.email;
-// };
