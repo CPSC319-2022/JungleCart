87ec50376fc42e838767db7d5d75ed2e6db0ab80
Raphael Deonova <raphaeldeonova@gmail.com>
2023-04-17 13:06:22 -0700

    add prompt for analytics

--- frontend/src/pages/seller/index.jsx
+++ frontend/src/pages/seller/index.jsx
@@ -19,6 +19,7 @@ const SellerDashboard = () => {
   return (
     <main>
       <section>
+        
         <div className={styles.bottom_container}>
           <div className="card w-96 shadow-xl image-full">
             <figure><Image src={GorillaIllustration} alt="Shoes" fill className='rounded-xl'/></figure>
@@ -32,6 +33,13 @@ const SellerDashboard = () => {
           </div>
           <div className={`${styles.card} ${styles.user_card}`}>
             <h2 className='font-bold text-2xl'>Analytics</h2>
+            <div className={"flex gap-x-2 py-1 items-center"}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" className="inline-block w-8 h-8 ">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+              </svg>
+              <p className={"text-xs "}>Analytics panel is not part of our MVP, but we have plans to implement this to allow better user insight </p>
+            </div>
             <Pulser />
           </div>
           
